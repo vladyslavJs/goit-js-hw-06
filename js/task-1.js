9a2f9266c82b9b4bde32971609870985595dde21
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-18 13:28:22 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -6,24 +6,23 @@ const customer = {
   balance: 24000,
   discount: 0.1,
   orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
+  
   getBalance() {
-    return balance;
+    return this.balance;
   },
   getDiscount() {
-    return discount;
+    return this.discount;
   },
   setDiscount(value) {
-    discount = value;
+    this.discount = value;
   },
   getOrders() {
-    return orders;
+    return this.orders;
   },
   addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
-  // Change code above this line
 };
 
 customer.setDiscount(0.15);
