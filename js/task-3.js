71e59248405d3f891e09bb6e3c660af8588426dd
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-18 14:28:16 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,32 @@
 // Задача 3. Конструктор рядків
 // Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
 
+class StringBuilder {
+    #value = 'initialValue';
+
+    constructor(value) {
+        this.#value = value;
+    }
+    
+    getValue() {
+        return this.#value;
+    }
+
+    padEnd(str) {
+        this.#value += str;
+    }
+
+    padStart(str) {
+        this.#value = str + this.#value;
+
+    }
+
+    padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+    }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
