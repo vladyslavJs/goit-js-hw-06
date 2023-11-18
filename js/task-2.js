b97f67abebf129153860162c3e64f21008267314
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-18 13:28:26 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,30 @@
 // Задача 2. Склад
 // Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
 
+class Storage {
+  #items = [];
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemRemove) {
+    this.#items = this.#items.filter(product => product !== itemRemove);
+    // this.#items.splice(1, 1);
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
+storage.removeItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
